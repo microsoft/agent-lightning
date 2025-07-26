3be55ad86887610524ec897ff248106bba26e98f
Yuge Zhang <scottyugochang@gmail.com>
2025-07-26 14:23:59 +0800

    cut down trainers

--- scripts/restart_ray.sh
+++ scripts/restart_ray.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-ray stop --force --grace-period 60 -v
+ray stop -v --force --grace-period 0
 env RAY_DEBUG=legacy HYDRA_FULL_ERROR=1 VLLM_USE_V1=1 ray start --head --dashboard-host=0.0.0.0
