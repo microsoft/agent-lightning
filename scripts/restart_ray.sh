15a8d6e94b683bbcb5c7abacd9da75b4b1db9798
Yuge Zhang <scottyugochang@gmail.com>
2025-07-26 14:45:58 +0800

    .

--- scripts/restart_ray.sh
+++ scripts/restart_ray.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-ray stop -v --force --grace-period 0
+ray stop -v --force --grace-period 60
 env RAY_DEBUG=legacy HYDRA_FULL_ERROR=1 VLLM_USE_V1=1 ray start --head --dashboard-host=0.0.0.0
