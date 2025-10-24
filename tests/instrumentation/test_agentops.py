3d8a73916b486f5ca04f4b11c4f7afa925af3efe
Hao Ni (CSI Interfusion Co Ltd) <v-nhao@microsoft.com>
2025-10-24 15:11:27 +0800

    add patched exporter test

---
+++ tests/instrumentation/test_agentops.py
@@ -0,0 +1,64 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from unittest.mock import MagicMock, patch
+
+from opentelemetry.sdk.metrics.export import MetricExportResult
+from opentelemetry.sdk.trace.export import SpanExportResult
+
+from agentlightning.instrumentation.agentops import (
+    SwitchableAuthenticatedOTLPExporter,
+    SwitchableOTLPMetricExporter,
+    SwitchableOTLPSpanExporter,
+    set_switch,
+)
+
+
+def test_switchable_authenticated_exporter():
+    switchable_authenticated_exporter = SwitchableAuthenticatedOTLPExporter(endpoint="http://dummy", jwt="dummy")
+
+    with patch.object(
+        switchable_authenticated_exporter.__class__.__bases__[0], "export", return_value=SpanExportResult.SUCCESS
+    ) as mock_export:
+        set_switch(True)
+        result = switchable_authenticated_exporter.export([])
+        assert result == SpanExportResult.SUCCESS
+        mock_export.assert_called_once()
+
+        set_switch(False)
+        result = switchable_authenticated_exporter.export([])
+        assert result == SpanExportResult.SUCCESS
+        assert mock_export.call_count == 1
+
+
+def test_switchable_otlp_metric_exporter():
+
+    switchable_otlp_metric_exporter = SwitchableOTLPMetricExporter()
+    with patch.object(
+        switchable_otlp_metric_exporter.__class__.__bases__[0], "export", return_value=MetricExportResult.SUCCESS
+    ) as mock_export:
+        set_switch(True)
+        result = switchable_otlp_metric_exporter.export(metrics_data=MagicMock())
+        assert result == MetricExportResult.SUCCESS
+        mock_export.assert_called_once()
+
+        set_switch(False)
+        result = switchable_otlp_metric_exporter.export(metrics_data=MagicMock())
+        assert result == MetricExportResult.SUCCESS
+        assert mock_export.call_count == 1
+
+
+def test_switchable_otlp_span_exporter():
+
+    switchable_otlp_span_exporter = SwitchableOTLPSpanExporter()
+    with patch.object(
+        switchable_otlp_span_exporter.__class__.__bases__[0], "export", return_value=SpanExportResult.SUCCESS
+    ) as mock_export:
+        set_switch(True)
+        result = switchable_otlp_span_exporter.export([])
+        assert result == SpanExportResult.SUCCESS
+        mock_export.assert_called_once()
+
+        set_switch(False)
+        result = switchable_otlp_span_exporter.export([])
+        assert result == SpanExportResult.SUCCESS
+        assert mock_export.call_count == 1
