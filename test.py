5fd14578d0ac8d8dd87c6c5f3d76435ec2e90976
Ekin Bozyel <ekin.bozyel@portx.net>
2026-02-01 21:20:54 +0300

    delete test file

--- test.py
+++
@@ -1,8 +0,0 @@
-     
-model="groq-meta-llama/llama-4-maverick-17b-128e-instruct"
-
-parts = model.split("-", 1)
-provider = parts[0]
-actual_model = parts[1] if len(parts) > 1 else model
-
-print(actual_model)
