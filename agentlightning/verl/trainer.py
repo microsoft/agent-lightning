436071f3ee985dbad91eac6161630a679c873221
Yunkai <yunkai.yyk@gmail.com>
2026-02-18 18:45:27 +0000

    fix: pass gts argument in _dump_generations call in _train_step

--- agentlightning/verl/trainer.py
+++ agentlightning/verl/trainer.py
@@ -417,10 +417,14 @@ class AgentLightningTrainer(RayPPOTrainer):
                     print(batch.batch.keys())
                     inputs = self.tokenizer.batch_decode(batch.batch["prompts"], skip_special_tokens=True)
                     outputs = self.tokenizer.batch_decode(batch.batch["responses"], skip_special_tokens=True)
+                    sample_gts = [
+                        item.non_tensor_batch.get("reward_model", {}).get("ground_truth", None) for item in batch
+                    ]
                     scores = batch.batch["token_level_scores"].sum(-1).cpu().tolist()
                     self._dump_generations(
                         inputs=inputs,
                         outputs=outputs,
+                        gts=sample_gts,
                         scores=scores,
                         reward_extra_infos_dict=reward_extra_infos_dict,
                         dump_path=rollout_data_dir,
